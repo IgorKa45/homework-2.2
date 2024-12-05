74b64de2ae4e71ab59cab242050514a106f8cb58
IgorKa45 <igorkanasnik@gmail.com>
2024-12-05 19:10:15 +0400

    Сделал дз 5 (Исключения в Java)

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -30,4 +30,53 @@ public class SearchEngine {
         }
         return results;
     }
+
+    public Searchable findMostRelevant(String search) throws BestResultNotFound {
+        if (search == null || search.isBlank()) {
+            throw new IllegalArgumentException("Поисковая строка не может быть пустой или null");
+        }
+        // максимальное количество вхождений строки
+        Searchable mostRelevant = null;
+        // максимальное количество вхождений строки на текущий момент
+        int maxOccurrences = 0;
+
+        for (Searchable searchable : searchables) {
+            if (searchable != null) {
+                String searchTerm = searchable.getSearchTerm().toLowerCase();
+                String lowerSearch = search.toLowerCase();
+
+                // Считаем количество вхождений строки search в searchTerm
+                int occurrences = 0;
+                int index = 0;
+                int substringIndex = searchTerm.indexOf(lowerSearch, index);
+
+                while (substringIndex != -1) {
+                    occurrences++;
+                    index = substringIndex + lowerSearch.length();
+                    substringIndex = searchTerm.indexOf(lowerSearch, index);
+                }
+
+                // Сравниваем текущие вхождения с максимальными
+                if (occurrences > maxOccurrences) {
+                    maxOccurrences = occurrences;
+                    mostRelevant = searchable;
+                }
+            }
+        }
+        if (mostRelevant == null) {
+            throw new BestResultNotFound("Для запроса \"" + search + "\" не найдено подходящего результата.");
+        }
+        return mostRelevant;
+    }
+
+    public class BestResultNotFound extends Exception {
+        public BestResultNotFound() {
+            super();
+        }
+
+        public BestResultNotFound(String message) {
+            super(message);
+        }
+    }
+
 }
